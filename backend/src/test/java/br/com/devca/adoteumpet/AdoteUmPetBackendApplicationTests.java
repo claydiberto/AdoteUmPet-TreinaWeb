fb3d6758d5f391d667fcc42266b8bdd5c377a91f
claydiberto <claydiberto@gmail.com>
2022-06-05 14:55:49 -0300

    Add backend project

---
+++ backend/src/test/java/br/com/devca/adoteumpet/AdoteUmPetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devca.adoteumpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteUmPetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
